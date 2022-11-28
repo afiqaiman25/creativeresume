97fcfde824ba530188bb9462af71273eaefdb729
afiqaiman25 <77537272+afiqaiman25@users.noreply.github.com>
2022-11-29 04:17:11 +0800

    Update ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -10,7 +10,7 @@ import "./ContactComponent.css";
 import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const blogSection = contactPageData.blogSection;
+
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 
